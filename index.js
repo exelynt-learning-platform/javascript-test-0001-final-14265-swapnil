8e7048c00b6a182e7614fd8f16f8716612b7e509
Swapnil Mohite <swapnilmohite9565@gmail.com>
2026-03-14 13:48:21 +0530

    Final:Solution for javascript-test-0001

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-let n = 5;
+function printPattern(n) {
+
+  for (let i = 1; i <= n; i++) {
+    let row = "";
 
 
-for (let i = 1; i <= n; i++) {
-  let row = "";
-
-  for (let j = 1; j <= n - i; j++) {
-    row += " ";
-  }
-
-  for (let j = 1; j <= (2 * i - 1); j++) {
-    if (j === 1 || j === (2 * i - 1)) {
-      row += "*";
-    } else {
+    for (let j = 1; j <= n - i; j++) {
       row += " ";
     }
+
+  
+    for (let j = 1; j <= (2 * i - 1); j++) {
+      if (j === 1 || j === (2 * i - 1)) {
+        row += "*";
+      } else {
+        row += " ";
+      }
+    }
+
+    console.log(row);
   }
 
-  console.log(row);
-}
+ 
+  for (let i = n - 1; i >= 1; i--) {
+    let row = "";
 
-
-for (let i = n - 1; i >= 1; i--) {
-  let row = "";
-
-  for (let j = 1; j <= n - i; j++) {
-    row += " ";
-  }
-
-  for (let j = 1; j <= (2 * i - 1); j++) {
-    if (j === 1 || j === (2 * i - 1)) {
-      row += "*";
-    } else {
+    for (let j = 1; j <= n - i; j++) {
       row += " ";
     }
-  }
 
-  console.log(row);
+    for (let j = 1; j <= (2 * i - 1); j++) {
+      if (j === 1 || j === (2 * i - 1)) {
+        row += "*";
+      } else {
+        row += " ";
+      }
+    }
+
+    console.log(row);
+  }
 }
+
+printPattern(5);
